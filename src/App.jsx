1ef062e35f88a23969150aa46f53424e9a899a3f
Rawda mohamed <Rawdamohamed618@gmail.com>
2026-07-02 04:11:37 +0300

    fix: resolve path case sensitivity for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
-import Navbar from "./components/common/Navbar.jsx";
-import Projects from "./pages/Projects.jsx";
-import Skills from "./pages/Skills.jsx";
-import Contact from "./pages/Contact.jsx";
-import Home from "./pages/Home.jsx";
-import AnimatedBg from "./components/AnimatedBg/AnimatedBg.jsx";
-import Footer from "./components/common/Footer.jsx";
+import Navbar from "@/components/common/Navbar.jsx";
+import Projects from "@/pages/Projects.jsx";
+import Skills from "@/pages/Skills.jsx";
+import Contact from "@/pages/Contact.jsx";
+import Home from "@/pages/Home.jsx";
+import AnimatedBg from "@/components/AnimatedBg/AnimatedBg.jsx";
+import Footer from "@/components/common/Footer.jsx";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import AboutMe from "./pages/AboutMe.jsx";
-import Experience from "./pages/Experience.jsx";
+import AboutMe from "@/pages/AboutMe.jsx";
+import Experience from "@/pages/Experience.jsx";
 import { useTranslation } from "react-i18next";
 import { useThemeStore } from "@/store/useThemeStore";
 
